a5599d4e164b360f02a69cae54dac24c41ee7c3d
nettenegri <nettenegri@gmail.com>
2022-11-30 17:23:36 -0700

    commit tree eecc89e300146659ec98372b465e9b650796a7ee

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 32;
+const num4 = 8;
+const mod = 32 / 8;
+
+let max = Math.max(15, 16, 17, 18, 19, 20);
